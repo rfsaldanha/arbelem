4f221e5738a863c5307778361242e2744306eaa1
Raphael Saldanha <rfsaldanha@gmail.com>
2025-10-30 15:47:22 -0300

    First version

---
+++ app.R
@@ -0,0 +1,130 @@
+# Packages
+library(shiny)
+library(bslib)
+library(dplyr)
+library(lubridate)
+library(kunakapir)
+library(vchartr)
+
+# Interface
+ui <- page_navbar(
+  title = "Qualidade do Ar - Belém COP30",
+  theme = bs_theme(bootswatch = "shiny"),
+
+  # Logo
+  tags$head(
+    tags$script(
+      HTML(
+        '$(document).ready(function() {
+             $(".navbar .container-fluid")
+               .append("<img id = \'myImage\' src=\'logos.png\' align=\'right\' height = \'70px\'>"  );
+            });'
+      )
+    ),
+    tags$style(
+      HTML(
+        '@media (max-width:992px) { #myImage { position: fixed; right: 10%; top: 0.5%; }}'
+      )
+    )
+  ),
+
+  # Translation
+  tags$script(
+    HTML(
+      "
+      $(document).ready(function() {
+        // Change the text 'Expand' in all tooltips
+        $('.card.bslib-card bslib-tooltip > div').each(function() {
+          if ($(this).text().includes('Expand')) {
+            $(this).text('Expandir');
+          }
+        });
+  
+        // Use MutationObserver to change the text 'Close'
+        var observer = new MutationObserver(function(mutations) {
+          $('.bslib-full-screen-exit').each(function() {
+            if ($(this).html().includes('Close')) {
+              $(this).html($(this).html().replace('Close', 'Fechar'));
+            }
+          });
+        });
+  
+        // Observe all elements with the class 'card bslib-card'
+        $('.card.bslib-card').each(function() {
+          observer.observe(this, { 
+            attributes: true, 
+            attributeFilter: ['data-full-screen'] 
+          });
+        });
+      });
+    "
+    )
+  ),
+
+  # Map page
+  nav_panel(
+    title = "Painel",
+
+    # Card
+    card(
+      full_screen = TRUE,
+      card_body(
+        vchartOutput(outputId = "graph")
+      )
+    )
+  ),
+
+  # About page
+  nav_panel(
+    title = "Sobre",
+    card(
+      card_header("Card title"),
+      p("Bla bla bla.")
+    ),
+    accordion(
+      multiple = FALSE,
+      accordion_panel(
+        "Título A",
+        p("Bla bla bla.")
+      ),
+      accordion_panel(
+        "Título B",
+        p("Bla bla bla.")
+      ),
+      accordion_panel(
+        "Título C",
+        p("Bla bla bla.")
+      )
+    )
+  )
+)
+
+# Server
+server <- function(input, output, session) {
+  output$graph <- renderVchart({
+    invalidateLater(millis = 600000, session = session)
+
+    # Get data
+    res <- get_reads_multiple_until(
+      device_id = "0325280630",
+      elements_ids = c("PM2.5", "O3 GCc"),
+      ts = Sys.time(),
+      number = 144 * 2
+    )
+
+    # Adjust
+    res <- res |>
+      arrange(ts) |>
+      mutate(ts = as_datetime(ts))
+
+    vchart(res) |>
+      v_line(
+        aes(x = ts, y = value, color = sensor_tag),
+        lineLabel = list(visible = TRUE)
+      ) |>
+      v_scale_y_continuous(min = 0) |>
+      v_specs_legend(visible = FALSE)
+  })
+}
+
+shinyApp(ui, server)
